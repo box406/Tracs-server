6c3376e532b1b8fbcc3a8810d5c1f5fdc6101a09
@box406 <box406@gmail.com>
2014-01-08 08:35:23 +0900

    tracの登録処理がとりあえず完成

--- sandbox/app/controllers/TracController.php
+++ sandbox/app/controllers/TracController.php
@@ -10,14 +10,14 @@ class TracController extends BaseController {
                                                 ->join('trac_areas', 'tracs.status', '=', 'trac_areas.id')
                                                 ->get(); 
         
-        // get database from accuracys 
+        // get database from accuracys & shaping 
         $original_accuracy_list = DB::table('accuracys')->get();
         $accuracy_list = [];
         foreach ($original_accuracy_list as $suffix => $value) {
             $accuracy_list["accuracy"][$value->id] = $value->accuracy;
         }
 
-        // shaping
+        // shaping $trac_info
         foreach($original_trac_info as $suffix => $value) {
         
             $trac_info[$value->trac_area][] = [
@@ -34,7 +34,20 @@ class TracController extends BaseController {
         return Response::json($trac_info);
     }
 
-    public function getAddTrac($id) {
-        echo "test-" . $id;
+    public function postAddTrac() {
+
+        DB::table('tracs')->insert(
+            array( 
+                "trac_id" => Input::json("trac_id"),
+                "status" => 1,
+                "trac_name" => Input::json("trac_name"),
+                "member_id" => Input::json("member_id"),
+                "accuracy_id" => Input::json("accuracy_id"),
+                "client_id" => Input::json("client_id")      
+            )
+        );
+
+        header("Access-Control-Allow-Origin: *");
+        return Response::json("respons ok"); 
     }
 }
